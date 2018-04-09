5a601b5b6c14198195ec32314f55b9faf056d90e
Babak <ssalimib@gmail.com>
2018-04-09 14:06:50 +0430

    Initial commit

---
+++ Assets/Scripts/Network/SFSconnect.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Requests;
+using Sfs2X.Entities;
+
+public class SFSconnect : MonoBehaviour {
+	public string configFile = "sfs-config.xml";
+	public bool UseConfigFile = true;
+	public string serverIP = "127.0.0.1";
+	public int serverPort = 9933;
+	public int defaultWsPort = 8080;
+	public string UserName = "bss";
+	public string Password = "";
+	public string ZoneName = "brawl";
+	public string RoomName = "arena";
+
+	SmartFox sfs;
+
+	void Start () 
+	{
+		
+		sfs = new SmartFox ();
+		sfs.ThreadSafeMode = true;
+		sfs.AddEventListener (SFSEvent.CONNECTION, OnConnection);
+		sfs.AddEventListener (SFSEvent.CONNECTION_LOST, OnConnectionLost);
+		sfs.AddEventListener (SFSEvent.LOGIN, OnLogin);
+		sfs.AddEventListener (SFSEvent.LOGIN_ERROR, onLoginError);
+		sfs.AddEventListener (SFSEvent.CONFIG_LOAD_SUCCESS, onConfigLoad);
+		sfs.AddEventListener (SFSEvent.CONFIG_LOAD_FAILURE, onConfigFail);
+		sfs.AddEventListener (SFSEvent.ROOM_JOIN, OnJoinRoom);
+		sfs.AddEventListener (SFSEvent.ROOM_JOIN_ERROR, OnJoinRoomError);
+		sfs.AddEventListener (SFSEvent.PUBLIC_MESSAGE, PublicMessageHandler);
+
+		if (UseConfigFile) 
+		{
+			//var cfg = Resources.Load<TextAsset>(configFile);
+			sfs.LoadConfig (Application.dataPath + "/Resources/sfs-config.xml", true);
+
+		}
+		else 
+		{
+			sfs.Connect (serverIP, serverPort);
+		}
+	}
+
+	void Update () 
+	{
+		sfs.ProcessEvents ();	
+	}
+
+	void PublicMessageHandler(BaseEvent e)
+	{
+		Debug.Log ("PublicMessage");
+		Room room = (Room)e.Params ["room"];
+		User sender = (User)e.Params ["sender"];
+		Debug.Log("[" + room.Name + "] " + sender.Name + ": " + e.Params["message"]); 
+	}
+
+	void OnJoinRoom(BaseEvent e)
+	{
+		Debug.Log ("Joined Room: " + e.Params ["room"]);
+		sfs.Send(new PublicMessageRequest("Hello World"));
+	}
+	void OnJoinRoomError(BaseEvent e)
+	{
+		Debug.Log ("Join room failed! ErrorCode:" + e.Params ["error"] + " - " + e.Params ["errorMessage"]);
+	}
+
+	void OnConnectionLost(BaseEvent e)
+	{
+		Debug.Log ("Connection lost!");
+	}
+
+	void onConfigLoad(BaseEvent e)
+	{
+		Debug.Log ("Loaded Config File Successfully");
+		sfs.Connect (sfs.Config.Host, sfs.Config.Port);
+	}
+	void onConfigFail(BaseEvent e)
+	{
+		Debug.Log ("Loading Config File Failed!");
+	}
+
+	void OnLogin(BaseEvent e)
+	{
+		Debug.Log ("Successfully loged into zone " +  e.Params["user"]);
+		sfs.Send (new JoinRoomRequest (RoomName));
+	}
+	void onLoginError(BaseEvent e)
+	{
+		Debug.Log ("Failed to login! Errore Code:"+e.Params["errorCode"]+" - "+e.Params["errorMessage"]);
+	}
+
+	void OnConnection(BaseEvent e)
+	{
+		if ((bool)e.Params ["success"]) 
+		{
+			Debug.Log ("Successfully connected");
+			if (UseConfigFile)
+				ZoneName = sfs.Config.Zone;
+			sfs.Send (new LoginRequest (UserName, Password, ZoneName));
+		}
+		else
+			Debug.Log ("Failed to connect");
+	}
+
+	void OnApplicationOutput()
+	{
+		if (sfs.IsConnected)
+			sfs.Disconnect ();
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 100f, jumpVelocity = 100f;
+	public bool isGrounded = false;
+	Rigidbody2D myBody;
+	Transform myTrans;
+	public LayerMask layerMask;
+	GameObject ground;
+	Transform groundTrans;
+	Animator animator;
+
+	// Use this for initialization
+	void Start () 
+	{
+		myBody = GetComponent<Rigidbody2D> ();	
+		myTrans = GetComponent<Transform> ();
+		groundTrans = GameObject.Find (this.name + "/isGrounded").transform;
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		isGrounded = Physics2D.Linecast (myTrans.position, groundTrans.position, layerMask);
+		move(Input.GetAxisRaw("Horizontal"));
+		if (Input.GetButtonDown ("Vertical"))
+		{
+			jump ();
+		}
+
+		if (myBody.velocity.x == 0 && myBody.velocity.y == 0)
+			animator.SetBool ("Running", false);
+		else
+			animator.SetBool ("Running", true);
+		//animator.SetBool ("Running", !isGrounded);
+	}
+	
+	public void move(float input)
+	{
+		Vector2 moveVel = myBody.velocity;
+		moveVel.x = speed * input;
+		myBody.velocity = moveVel;
+	}
+
+	public void jump()
+	{
+		if(isGrounded)
+			myBody.velocity += jumpVelocity * Vector2.up;
+	}
+}
