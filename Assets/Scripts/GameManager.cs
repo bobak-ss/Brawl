583b44b0b92d2884c199067b8c4f3cd91e0590d0
Babak <ssalimib@gmail.com>
2018-04-29 17:09:11 +0430

    So much have been done too lazy to separate commits

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,244 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Entities;
+using Sfs2X.Entities.Data;
+using Sfs2X.Entities.Variables;
+using Sfs2X.Requests;
+using System;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour 
+{
+    //----------------------------------------------------------
+    // Public properties
+    //----------------------------------------------------------
+
+    public GameObject localPlayerObj;
+    public GameObject remotePlayerObj;
+    public LayerMask layerMask;
+    public float speed = 100f, jumpVelocity = 50f;
+
+    //----------------------------------------------------------
+    // Private properties
+    //----------------------------------------------------------
+
+    private SmartFox sfs;
+    private GameObject localPlayer = null;
+    private bool localPlayerIsGrounded = true;
+	private Rigidbody2D localPlayerBody = null;
+    private Transform localPlayerTrans = null;
+    private Transform localPlayerGround = null;
+    private Animator localPlayerAnimator = null;
+    private GameObject remotePlayer = null;
+    //private bool remotePlayerIsGrounded = true;
+    private Rigidbody2D remotePlayerBody = null;
+    //private Transform remotePlayerTrans = null;
+    //private Transform remotePlayerGround = null;
+    private Animator remotePlayerAnimator = null;
+    static private Text log;
+
+    //----------------------------------------------------------
+    // Unity calback methods
+    //----------------------------------------------------------
+        
+    void Start()
+    {
+        log = GameObject.Find("LogsObject").AddComponent<Text>();
+
+        if (!SmartFoxConnection.IsInitialized)
+        {
+            trace("SFS not initialized!");
+            SceneManager.LoadScene("Connection");
+			return;
+        }
+
+        sfs = SmartFoxConnection.Connection;
+
+        if(sfs.IsConnected)
+        {
+            trace("sfs is connected in game manager");
+            
+            sfs.AddEventListener(SFSEvent.USER_ENTER_ROOM, UserEnterRoomHandler);
+            sfs.AddEventListener(SFSEvent.USER_EXIT_ROOM, UserExitRoomHandler);
+            sfs.AddEventListener(SFSEvent.USER_VARIABLES_UPDATE, UserVarUpdateHandler);
+            sfs.AddEventListener(SFSEvent.PUBLIC_MESSAGE, PublicMessageHandler);
+            sfs.AddEventListener(SFSEvent.OBJECT_MESSAGE, MessageHandler);
+            sfs.AddEventListener(SFSEvent.CONNECTION_LOST, LostConnectionHandler);
+        }
+
+        if (localPlayer == null)
+        {
+            SpawnLocalPlayer();
+
+            // Initializations
+            localPlayerBody = localPlayer.GetComponent<Rigidbody2D>();
+            localPlayerTrans = localPlayer.GetComponent<Transform>();
+            localPlayerAnimator = localPlayer.GetComponent<Animator>();
+            localPlayerGround = GameObject.Find(localPlayer.name + "/isGrounded").transform;
+        }
+    }
+
+    void Update()
+    {
+        if(sfs != null)
+        {
+            sfs.ProcessEvents();
+            if (localPlayer != null)
+            {
+                localPlayerIsGrounded = Physics2D.Linecast(localPlayerTrans.position, localPlayerGround.position, layerMask);
+
+                move(CrossPlatformInputManager.GetAxisRaw("Horizontal"));
+                if (CrossPlatformInputManager.GetButtonDown("Jump"))
+                    jump();
+
+                if (localPlayerBody.velocity.x == 0 && localPlayerBody.velocity.y == 0)
+                    localPlayerAnimator.SetBool("Running", false);
+                else
+                    localPlayerAnimator.SetBool("Running", true);
+            }
+            if (remotePlayer != null)
+            {
+                if (remotePlayerBody.velocity.x == 0 && remotePlayerBody.velocity.y == 0)
+                    remotePlayerAnimator.SetBool("Running", false);
+                else
+                    remotePlayerAnimator.SetBool("Running", true);
+            }
+        }
+    }
+
+    // ------------------------ Player Spawn Methods ------------------------
+    private void SpawnLocalPlayer()
+    {
+        localPlayer = GameObject.Instantiate(localPlayerObj);
+        localPlayer.transform.position = new Vector3(UnityEngine.Random.Range(-190f, 190f), 150f, 0);
+
+        List<UserVariable> userVariables = new List<UserVariable>();
+        userVariables.Add(new SFSUserVariable("posx", (double)localPlayer.transform.position.x));
+        userVariables.Add(new SFSUserVariable("posy", (double)localPlayer.transform.position.y));
+        sfs.Send(new SetUserVariablesRequest(userVariables));
+    }
+    private void SpawnRemotePlayer(SFSUser user)
+    {
+        remotePlayerObj.transform.position = new Vector3( (float)user.GetVariable("posx").GetDoubleValue(), (float)user.GetVariable("posy").GetDoubleValue() );
+        remotePlayer = GameObject.Instantiate(remotePlayerObj);
+
+        // Initializations
+        remotePlayerBody = remotePlayer.GetComponent<Rigidbody2D>();
+        //remotePlayerTrans = remotePlayer.GetComponent<Transform>();
+        remotePlayerAnimator = remotePlayer.GetComponent<Animator>();
+        //remotePlayerGround = GameObject.Find(remotePlayer.name + "/isGrounded").transform;
+    }
+
+
+    // ------------------------ Player Action Methods ------------------------
+    public void move(float input)
+    {
+        Vector2 moveVel = localPlayerBody.velocity;
+        moveVel.x = speed * input;
+        localPlayerBody.velocity = moveVel;
+
+        List<UserVariable> userVariables = new List<UserVariable>();
+        userVariables.Add(new SFSUserVariable("velx", (double)moveVel.x));
+        userVariables.Add(new SFSUserVariable("vely", (double)moveVel.y));
+        sfs.Send(new SetUserVariablesRequest(userVariables));
+    }
+    public void jump()
+    {
+        Vector2 moveVel = localPlayerBody.velocity;
+        
+        if (localPlayerIsGrounded)
+            moveVel = new Vector3(localPlayerBody.velocity.x, localPlayerBody.velocity.y + jumpVelocity);
+        localPlayerBody.velocity = moveVel;
+        
+        List<UserVariable> userVariables = new List<UserVariable>();
+        userVariables.Add(new SFSUserVariable("vely", (double)moveVel.y));
+        sfs.Send(new SetUserVariablesRequest(userVariables));
+    }
+
+    //----------------------------------------------------------
+    // SmartFoxServer event listeners
+    //----------------------------------------------------------
+
+    private void UserEnterRoomHandler(BaseEvent evt)
+    {
+        SFSUser user = (SFSUser)evt.Params["user"];
+        trace("(" + user.Name + ") Entered the room!");
+
+        // we set local player vars to let new entered user
+        List<UserVariable> userVariables = new List<UserVariable>();
+        userVariables.Add(new SFSUserVariable("posx", (double)localPlayer.transform.position.x));
+        userVariables.Add(new SFSUserVariable("posy", (double)localPlayer.transform.position.y));
+        sfs.Send(new SetUserVariablesRequest(userVariables));
+
+        SpawnRemotePlayer(user);
+    }
+
+    private void UserExitRoomHandler(BaseEvent evt)
+    {
+        SFSUser user = (SFSUser)evt.Params["user"];
+        trace("(" + user.Name + ") Exited the room!");
+        Destroy(remotePlayer);
+    }
+    
+    private void UserVarUpdateHandler(BaseEvent evt)
+    {
+        List<string> changedVars = (List<string>)evt.Params["changedVars"];
+
+        SFSUser user = (SFSUser)evt.Params["user"];
+
+        if (user == sfs.MySelf) return;
+
+        if (remotePlayer == null)
+            SpawnRemotePlayer(user);
+
+        //trace("(" + user.Name + ") Changed its vars!");
+        if (changedVars.Contains("posx") || changedVars.Contains("posy"))
+            remotePlayer.transform.position = new Vector3((float)user.GetVariable("posx").GetDoubleValue(), (float)user.GetVariable("posy").GetDoubleValue(), 0);
+        if (changedVars.Contains("velx"))
+            remotePlayer.GetComponent<Rigidbody2D>().velocity = new Vector3((float)user.GetVariable("velx").GetDoubleValue(), (float)user.GetVariable("vely").GetDoubleValue(), 0);
+        if (changedVars.Contains("vely"))
+            remotePlayer.GetComponent<Rigidbody2D>().velocity = new Vector3((float)user.GetVariable("velx").GetDoubleValue(), (float)user.GetVariable("vely").GetDoubleValue(), 0);
+    }
+
+    private void PublicMessageHandler(BaseEvent e)
+    {
+        //Debug.Log("!!! New PublicMessage !!!");
+        Room room = (Room)e.Params["room"];
+        User sender = (User)e.Params["sender"];
+        String msg = "[" + room.Name + "] " + sender.Name + ": " + e.Params["message"];
+        trace(msg);
+    }
+
+    private void LostConnectionHandler(BaseEvent evt)
+    {
+        trace("Lost Connection");
+        sfs.RemoveAllEventListeners();
+        SceneManager.LoadScene("Connection");
+    }
+
+    private void MessageHandler(BaseEvent evt)
+    {
+        trace("Message Handler");
+    }
+
+    public static Text trace(string textString)
+    {
+        Debug.Log(textString);
+        log.text += "\n-" + textString;
+
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        log.font = ArialFont;
+        log.material = ArialFont.material;
+        log.fontSize = 7;
+        log.color = new Color(0.058f, 0.450f, 0f);
+        log.verticalOverflow = VerticalWrapMode.Overflow;
+        log.alignByGeometry = true;
+
+        return log;
+    }
+}
---
+++ Assets/Scripts/Network/Connection.cs
@@ -0,0 +1,207 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sfs2X;
+using Sfs2X.Core;
+using Sfs2X.Requests;
+using Sfs2X.Entities;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System;
+
+public class Connection : MonoBehaviour {
+	public string configFile = "sfs-config.xml";
+	public bool UseConfigFile = true;
+	public string serverIP = "127.0.0.1";
+	public int serverPort = 9933;
+	public int defaultWsPort = 8080;
+	public string UserName = "bss";
+	public string Password = "";
+	public string ZoneName = "brawl";
+
+	public static SmartFox sfs;
+    
+    static private Text log;
+    private GameObject popUp;
+    private Text errorText;
+    //private Button okBtn;
+    private InputField inputField;
+
+
+    void Start ()
+    {
+        log = GameObject.Find("LogsObject").AddComponent<Text>();
+        popUp = GameObject.Find("PopUp");
+        errorText = GameObject.Find("ErrorTxt").GetComponent<Text>();
+        //okBtn = GameObject.Find("Button").GetComponent<Button>();
+        inputField = GameObject.Find("InputField").GetComponent<InputField>();
+        inputField.text = serverIP;
+
+        sfs = new SmartFox();
+
+        // Set ThreadSafeMode explicitly, or Windows Store builds will get a wrong default value (false)
+        sfs.ThreadSafeMode = true;
+
+        // SmartFox Event listeners
+        sfs.AddEventListener(SFSEvent.CONNECTION, ConnectionHandler);
+        sfs.AddEventListener(SFSEvent.CONNECTION_LOST, ConnectionLostHandler);
+        sfs.AddEventListener(SFSEvent.CONFIG_LOAD_SUCCESS, ConfigLoadHandler);
+        sfs.AddEventListener(SFSEvent.CONFIG_LOAD_FAILURE, ConfigLoadFailHandler);
+        sfs.AddEventListener(SFSEvent.LOGIN, LoginHandler);
+        sfs.AddEventListener(SFSEvent.LOGIN_ERROR, LoginErrorHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_ADD, RoomAddHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_CREATION_ERROR, RoomCreationErrorHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_JOIN, RoomJoinHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_JOIN_ERROR, RoomJoinErrorHandler);
+        
+        if (UseConfigFile)
+            sfs.LoadConfig(Application.dataPath + "/Resources/sfs-config.xml", true);
+        else
+            sfs.Connect(serverIP, serverPort);
+    }
+
+    void Update () 
+	{
+        if (sfs != null)
+            sfs.ProcessEvents();
+        else if (!popUp.activeInHierarchy)
+            popUp.SetActive(true);
+    }
+    public void startSmartfox()
+    {
+        sfs = new SmartFox();
+
+        // Set ThreadSafeMode explicitly, or Windows Store builds will get a wrong default value (false)
+        sfs.ThreadSafeMode = true;
+
+        // SmartFox Event listeners
+        sfs.AddEventListener(SFSEvent.CONNECTION, ConnectionHandler);
+        sfs.AddEventListener(SFSEvent.CONNECTION_LOST, ConnectionLostHandler);
+        sfs.AddEventListener(SFSEvent.CONFIG_LOAD_SUCCESS, ConfigLoadHandler);
+        sfs.AddEventListener(SFSEvent.CONFIG_LOAD_FAILURE, ConfigLoadFailHandler);
+        sfs.AddEventListener(SFSEvent.LOGIN, LoginHandler);
+        sfs.AddEventListener(SFSEvent.LOGIN_ERROR, LoginErrorHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_ADD, RoomAddHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_CREATION_ERROR, RoomCreationErrorHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_JOIN, RoomJoinHandler);
+        sfs.AddEventListener(SFSEvent.ROOM_JOIN_ERROR, RoomJoinErrorHandler);
+
+        serverIP = inputField.text;
+        if (UseConfigFile)
+            sfs.LoadConfig(Application.dataPath + "/Resources/sfs-config.xml", true);
+        else
+            sfs.Connect(serverIP, serverPort);
+
+        popUp.SetActive(false);
+    }
+    // Handlers
+
+    // Connection Handlers
+    void ConnectionHandler(BaseEvent e)
+    {
+        if ((bool)e.Params["success"])
+        {
+            trace("Successfully connected");
+            if (UseConfigFile)
+                ZoneName = sfs.Config.Zone;
+
+            // Save reference to the SmartFox instance in a static field, to share it among different scenes
+            SmartFoxConnection.Connection = sfs;
+
+            //startig login phase
+            sfs.Send(new LoginRequest(UserName, Password, ZoneName));
+        }
+        else
+        {
+            RetrySrvFail("Failed to connect");
+        }
+    }
+
+    void ConnectionLostHandler(BaseEvent e)
+    {
+        RetrySrvFail("Connection lost!");
+    }
+
+    // Config file load Handlers
+    void ConfigLoadHandler(BaseEvent e)
+    {
+        trace("Loaded Config File Successfully");
+        sfs.Connect(sfs.Config.Host, sfs.Config.Port);
+    }
+    void ConfigLoadFailHandler(BaseEvent e)
+    {
+        RetrySrvFail("Loading Config File Failed!");
+    }
+
+    // Login Handlers
+    void LoginHandler(BaseEvent e)
+    {
+        trace("Successfully logged into zone! user: " + e.Params["user"]);
+
+        string roomName = "arena";
+
+        // We either create the Game Room or join it if it exists already
+        if (sfs.RoomManager.ContainsRoom(roomName))
+        {
+            trace("GameRoom already exists! Joining the room");
+            sfs.Send(new JoinRoomRequest(roomName));
+        }
+        else
+        {
+            trace("GameRoom does not exists! creating the room... Joining the room...");
+            RoomSettings settings = new RoomSettings(roomName);
+            settings.MaxUsers = 2;
+            sfs.Send(new CreateRoomRequest(settings, true));
+        }
+    }
+    void LoginErrorHandler(BaseEvent e)
+    {
+        RetrySrvFail("Failed to login! Errore Code:" + e.Params["errorCode"] + " - " + e.Params["errorMessage"]);
+    }
+    // Room Handlers
+    private void RoomJoinHandler(BaseEvent e)
+    {
+        trace("Joined Room: " + e.Params["room"]);
+
+        // Remove SFS2X listeners
+        sfs.RemoveAllEventListeners();
+        
+        SceneManager.LoadScene("GameStaging");
+    }
+    private void RoomJoinErrorHandler(BaseEvent e)
+    {
+        RetrySrvFail("ERROR: Join room failed! ErrorCode:" + e.Params["error"] + " - " + e.Params["errorMessage"]);
+    }
+    private void RoomAddHandler(BaseEvent e)
+    {
+        RetrySrvFail("Adding room[" + e.Params["room"] + "] was successfull!");
+    }
+    private void RoomCreationErrorHandler(BaseEvent e)
+    {
+        RetrySrvFail("ERROR: Creating room[" + e.Params["room"] + "] was NOT successfull!\nErrorCode:" + e.Params["error"] + " - " + e.Params["errorMessage"]);
+    }
+
+    public static Text trace(string textString)
+    {
+        Debug.Log(textString);
+        log.text += "\n-" + textString;
+
+        Font ArialFont = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+        log.font = ArialFont;
+        log.material = ArialFont.material;
+        log.fontSize = 30;
+        log.color = new Color(0.058f, 1f, 0f);
+        log.verticalOverflow = VerticalWrapMode.Overflow;
+        log.alignByGeometry = true;
+
+        return log;
+    }
+
+    private void RetrySrvFail(String errorMsg)
+    {
+        trace(errorMsg);
+        errorText.text = errorMsg;
+        sfs.RemoveAllEventListeners();
+        popUp.SetActive(true);
+    }
+}
