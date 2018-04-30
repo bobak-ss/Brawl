f73c2384d38fdaeab99d5fa69f03444cde51fcb9
Babak <ssalimib@gmail.com>
2018-04-30 17:39:50 +0430

    Changed camera position to follow player

---
+++ Assets/Scripts/followPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followPlayer : MonoBehaviour {
+
+    public float depthOfView = -10;
+
+    private Transform cameraTrans;
+    private Transform playerTrans;
+
+	void Start () {
+        cameraTrans = GetComponent<Transform>();
+        playerTrans = GameObject.Find("localPlayer(Clone)").GetComponent<Transform>();
+	}
+	
+	void Update () {
+        cameraTrans.position = new Vector3(playerTrans.position.x, 0, depthOfView);
+	}
+}
