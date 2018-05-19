501f4e351364f7c629d29b8453e1408512b7f6e9
Babak <ssalimib@gmail.com>
2018-05-19 12:28:59 +0430

    Better level design

---
+++ Assets/Scripts/TiledBackground.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class TiledBackground : MonoBehaviour {
+
+	public int textureSize = 32;
+
+	public bool scaleHorizontialy = true;
+	public bool scaleVerticaly = true;
+
+	// Use this for initialization
+	void Start () {
+		var xTile = scaleHorizontialy ? Mathf.Ceil(transform.localScale.x / textureSize) : 1;
+		var yTile = scaleVerticaly ? Mathf.Ceil(transform.localScale.y / textureSize) : 1;
+
+		GetComponent<Renderer>().material.mainTextureScale = new Vector3 (xTile, yTile, 1);
+	}
+}
